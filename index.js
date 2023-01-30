5d77ced458acac656c5fcdc715ce6b81579224bd
jonas <jonas_hoerl@web.de>
2023-01-30 19:04:55 +0100

    move wishes from index.html to index.js

--- index.js
+++ index.js
@@ -1,17 +1,100 @@
 data = [{
-  "Name": "Insert description here.",
-  "Description": "379.99",
+  "Name": "Tress of the Emerald Sea",
+  "Description": "Das Buch wurde neben 3 anderen heimlich geschrieben und letztes Jahr bekannt gegeben. Leider kommt es erst im April raus. Ich freue mich aber auch über eine Vorbestellung",
+  "Image": "https://b1512865.smushcdn.com/1512865/wp-content/uploads/2023/01/00_Cover-scaled-400x600.jpg?lossy=1&strip=1&webp=1",
+  "Links": ["https://www.thalia.de/shop/home/artikeldetails/A1066947502?ProvID=11000533&gclid=CjwKCAiA2rOeBhAsEiwA2Pl7Q8VSfX7Gc9LhyjlMWT9LAEtDuhq33TzFq1wCynbQpieR3R4N4D2PIBoCTfQQAvD_BwE"]
+},
+{
+  "Name": "Hawaii Hemd",
+  "Description": "Dann kann ich dieses Jahr auf Elba selbstbewusst am Stand flanieren<br />Größe: M<br />Farbe: Marineblau & Gold",
   "Image": "https://m.media-amazon.com/images/I/81ebFoB6-9L._AC_UX569_.jpg",
-  "Links": ["https://m.media-amazon.com/images/I/81ebFoB6-9L._AC_UX569_.jpg"]
-}, {
-  "Name": "Insert description here.",
-  "Description": "379.99",
-  "Image": "https://m.media-amazon.com/images/I/81ebFoB6-9L._AC_UX569_.jpg",
-  "Links": ["https://m.media-amazon.com/images/I/81ebFoB6-9L._AC_UX569_.jpg"],
-  "Patron": "Mama"
-}, ]
+  "Links": ["https://www.amazon.de/-/en/dp/B083TCN1WY/ref=twister_B085VRN7PQ?_encoding=UTF8&th=1&psc=1"]
+},
+{
+  "Name": "Hello Fresh Gutschein",
+  "Description": "Hier bekommt man Rezepte passgenau zugeschickt und kann so ganz entspannt neue Gerichte kennenlernen",
+  "Image": "https://hf-website-live.s3-eu-west-1.amazonaws.com/be/cms/homepage/image_columns/hiw-3-v2.png",
+  "Links": ["https://www.hellofresh.de/gift"]
+},
+{
+  "Name": "Glatzen-Rasierer",
+  "Description": "Ansonten muss Hannah immer nochmal drüber schauen, ob ich nicht eine Ecke Haar-Unkraut übersehen habe - als ob man seinen Hinterkopf sehen könnte ...",
+  "Image": "https://m.media-amazon.com/images/I/61quaj0bKIL._AC_SL1080_.jpg",
+  "Links": ["https://www.amazon.de/dp/B07R7YXVJL/?tag=glv-21&ascsubtag=5652e7e4-4f89-4398-b78a-1686c29c3384&th=1&psc=1&linkCode=osi"]
+},
+{
+  "Name": "Hot Mamas Sauce No14",
+  "Description": "Das ist meine absolute Lieblingssooße. Die Flasche ist nur so klein, deshalb auch gerne mehrmals schenken",
+  "Image": "https://haendlmaier-shop.de/wp-content/uploads/2020/09/Hotmamas_No14_95ml_Neu-768x1144.jpg",
+  "Links": ["https://haendlmaier-shop.de/produkt/hotmamas-n-14-red-habanero-sauce-95ml/"]
+},
+{
+  "Name": "lustige Socken",
+  "Description": "Ach, ich mag bunte und lustige Socken. Ich habe mal ein paar Beispiele verlinkt, aber fühl dich dadurch nicht eingeschränkt",
+  "Image": "images\\lustigeSocken.jpg",
+  "Links": [
+    "https://manymornings.com/de/produkt/ragnasocks/",
+    "https://de.jimmylion.com/products/socken-modell-galaxy-dunkelgrau",
+    "https://www.dedoles.de/lustige-socken-chilischoten#product-designs"
+  ]
+},
+{
+  "Name": "2 Festival-Tickets",
+  "Description": "Ich fande das war eines der schönesten Festival. Vor allem weil es so klein war und so schön im Wlad gelegen ist.",
+  "Image": "https://shop-of-the-forest.de/media/image/3a/63/19/Sound-of-the-Forest-2016-Skyline-by-night-Heidi-Schultz-2_1280x1280.jpg",
+  "Links": ["https://sound-of-the-forest.de/tickets/"]
+},
+{
+  "Name": "Kochkurs",
+  "Description": "Gern auch für 2 Personen. Am liebsten vegetarisch und eine etwas ausgefallenere Küche wie asiatisch oder arabisch",
+  "Image": "https://aufgetischt.com/wp-content/uploads/2021/02/Kochkurse-2021.jpg",
+  "Links": []
+},
+{
+  "Name": "Magic Karten",
+  "Description": "Genauer ein ganzes Display (36 Booster) Khans of Thakir. Das ist meine absolute Lieblingsedition & mittlerweile schon ganze 8 Jahre alt.",
+  "Image": "images\\khan.jpg",
+  "Links": ["https://www.magic-games.at/shop/magic-the-gathering/khans-of-tarkir-booster-box-display-deutsch/"]
+},
+{
+  "Name": "2 kleine Küchenmesser",
+  "Description": "Um Obst und Gemüse schön klein zu schneiden. Nichts besonderes. Ihr werdet was finden.",
+  "Image": "https://www.ggs-bestecke.com/media/image/b7/8b/83/K-chenmesser-zweifarbig-s-ge-Gruppe-6-424_200x200.jpg",
+  "Links": []
+},
+{
+  "Name": "Maus",
+  "Description": "Meine Maus macht gerade immer Doppelklicks. Also wird jedes Programm 2x geöffnet",
+  "Image": "https://m.media-amazon.com/images/I/61KTBWy8kAL._AC_SS450_.jpg",
+  "Links": ["https://www.amazon.de/-/en/INPHIC-Bluetooth-Wireless-Mouse-Three-black/dp/B08BR6XLBT/ref=sr_1_4?crid=QFQYC63M9Q2N&keywords=bluetooth-maus&qid=1674895653&sprefix=bluetooth%2Bmaus%2Caps%2C103&sr=8-4&th=1"]
+},
+{
+  "Name": "PlayStation 5",
+  "Description": "Wichtig ist, dass es nicht die Digital-Edition ist. Da gibts nämlich kein Bluray-Laufwerk und dann kann Hannah dort kein Herr der Ringe schauen.",
+  "Image": "https://gmedia.playstation.com/is/image/SIEPDC/ps5-product-thumbnail-01-en-14sep21?$facebook$",
+  "Links": ["https://www.amazon.de/-/en/dp/B0BK9MC6QM/ref=sr_1_1?keywords=playstation+5&qid=1674895070&sprefix=play%2Caps%2C105&sr=8-1"]
+},
+{
+  "Name": "Dungeons, Dice & Danger",
+  "Description": "Das neueste Spiel vom Magic-Erfinder. Man würfelt und kämpft sich durch Monster-Dungeons",
+  "Image": "https://www.fantasywelt.de/media/image/product/144090/lg/dungeons-dice-and-danger-multilingual.jpg",
+  "Links": ["https://www.fantasywelt.de/Dungeons-Dice-and-Danger-Multilingual"]
+},
+{
+  "Name": "Fahrrad-Licht",
+  "Description": "GVolt 70.1: Das hat Anna, das hat Johannes - außerdem ist mein aktuelles ein bisschen kaputt",
+  "Image": "images\\gvolt2.jpg",
+  "Links": ["https://www.amazon.de/-/en/Cateye-Unisex-Adult-GVolt-Neutral/dp/B08BC4PP58/ref=sr_1_1?crid=2K2XZIQED5MHQ&keywords=gvolt+70&qid=1674407250&sprefix=gvolt+70%2Caps%2C105&sr=8-1"]
+},
+{
+  "Name": "Saugroboter",
+  "Description": "Habe mir noch nichts konkretes überlegt. Mir schwebt vor allem etwas Kompaktes für unsere kleine Wohnung vor",
+  "Image": "https://cdn.idealo.com/folder/Product/202218/9/202218905/s1_produktbild_gross/airrobo-p20.jpg",
+  "Links": []
+},
 
-data = []
+]
+
 
 
 createLinksHtml = links => {
