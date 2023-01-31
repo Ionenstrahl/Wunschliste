9f981fdd3c4c47411a53c2a874ce43b59d76cd59
jonas <jonas_hoerl@web.de>
2023-01-31 08:57:13 +0100

    replace arrow functions with functions

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+for (var i = 0; i < data.length; i++) {
+  const wish = data[i];
+  if (!wish["Patron"]) {
+    addWishHtml(wish)
+  } else {
+    addGratitude(wish)
+  }
+}
 
-createLinksHtml = links => {
+function addWishHtml(wish) {
+  var html = document.createElement('div');
+  html.className = 'col-lg-4 feature-box';
+  html.innerHTML = createWishInnerHtml(wish)
+
+  document.getElementById("wishes").appendChild(html);
+}
+
+function createWishInnerHtml(wish) {
+  var title = wish.Name;
+  var description = wish.Description;
+  var image = wish.Image;
+  var links = wish.Links;
+
+  return '<img src="' + image + '" class="present-img">' +
+    '<h3 class="feature-title">' + title + '</h3>' +
+    '<p>' + description + '</p>' +
+    createLinksHtml(links)
+}
+
+function createLinksHtml(links) {
   var linksHtml = ""
   for (const link of links) {
     linksHtml += '<a href="' + link + '" class="btn btn-secondary">Buy</a>'
@@ -7,33 +35,21 @@ createLinksHtml = links => {
   return linksHtml
 }
 
-addWishHtml = wish => {
-  var title = data[i].Name;
-  var description = data[i].Description;
-  var image = data[i].Image;
-  var links = data[i].Links;
-
-  var wish = document.createElement('div');
-
-  wish.className = 'col-lg-4 feature-box';
-  wish.innerHTML =
-    '<img src="' + image + '" class="present-img">' +
-    '<h3 class="feature-title">' + title + '</h3>' +
-    '<p>' + description + '</p>' +
-    createLinksHtml(links)
-
-  document.getElementById("wishes").appendChild(wish);
+function addGratitude(wish) {
+  var html = document.createElement('div');
+  html.className = 'col-3 col-lg-1';
+  html.innerHTML = createGratitudeInnerHtml(wish)
+  
+  document.getElementById("laudation").appendChild(html);
 }
 
-addGratitude = wish => {
-  var title = data[i].Name;
-  var description = data[i].Description;
-  var image = data[i].Image;
-  var patron = data[i].Patron;
+function createGratitudeInnerHtml(wish) {
+  var title = wish.Name;
+  var description = wish.Description;
+  var image = wish.Image;
+  var patron = wish.Patron;
 
-  var wish = document.createElement('div');
-  wish.className = 'col-3 col-lg-1';
-  wish.innerHTML = '<img src="' + image + '" class="gifted-img" type="button" data-toggle="modal" data-target="#' + image + '">' +
+  return '<img src="' + image + '" class="gifted-img" type="button" data-toggle="modal" data-target="#' + image + '">' +
     '<div class="modal fade" id="' + image + '" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">' +
     '<div class="modal-dialog" role="document">' +
     '<div class="modal-content">' +
@@ -49,15 +65,4 @@ addGratitude = wish => {
     '</div>' +
     '</div>' +
     '</div>'
-  document.getElementById("laudation").appendChild(wish);
-}
-
-for (var i = 0; i < data.length; i++) {
-  const wish = data[i];
-  if (!!wish["Patron"]) {
-    addGratitude(wish)
-  } else {
-    addWishHtml(wish)
-  }
-
 }
